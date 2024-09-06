bf40454fb5858cb364c69e16ff21c52f9ac75057
Spring-Rebirth <cjunwei6249@gmail.com>
2024-09-06 19:14:42 +0800

    跟随视频的代码补充

--- components/local/Modals.tsx
+++ components/local/Modals.tsx
@@ -1,6 +1,17 @@
+"use client";
 import { CreateWorkspaceModal } from "./createWorkspaceModal";
+import { use, useState } from "react";
+import { useEffect } from "react";
 
 const Modals = () => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
   return (
     <>
       <CreateWorkspaceModal />
